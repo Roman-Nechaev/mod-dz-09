08ae8f7e67d6ce54e27aff921699f2effe1a768c
Roman-Nechaev <89315040+Roman-Nechaev@users.noreply.github.com>
2022-08-25 07:32:50 +0000

    Deploying to gh-pages from @ Roman-Nechaev/mod-dz-09@1dd34044d66d3030904ad727038f2b508cc100ad 🚀

--- 01-color-switcher.7c7f2959.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(t){setInterval(n,1e3)})),e.addEventListener("click",(function(t){clearInterval(""),console.log("BtnStop")}));function n(t){console.log("start")}}();
-//# sourceMappingURL=01-color-switcher.7c7f2959.js.map
