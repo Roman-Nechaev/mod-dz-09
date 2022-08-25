37f0332651b885b4e649d4120eee099cccbd91a3
Roman-Nechaev <nrchaevroman@gmail.com>
2022-08-25 09:38:06 +0300

    pr

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+//  <button type="button" data-start>Start</button>
+//     <button type="button" data-stop>Stop</button>
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+btnStart.addEventListener('click', onbtnStart);
+btnStop.addEventListener('click', onBtnStop);
+let intervalId = '';
+function onbtnStart(evt) {
+  onIntervsl();
+  // console.log('btnStart:', evt.target);
+}
+
+function onIntervsl() {
+  const intervalId = setInterval(name, 1000);
+}
+function onBtnStop(evt) {
+  clearInterval(intervalId);
+  console.log('BtnStop');
+}
+
+function name(params) {
+  console.log('start');
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,29 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+// const timer = {
+//   start() {
+//     const startTime = Date.now();
+
+//     setInterval(() => {
+//       const currentTime = Date.now();
+//       console.log(currentTime - startTime);
+//     }, 1000);
+//   },
+// };
+
+// timer.start();
+
+const mass = ['Roman', 'Ragnar', 'Thor', 'Odin']; // массив объектов
+const promises = mass.map(prom); // возвращает массив объектов
+// const promises = mass.map(mas => prom(mas)); // идентичная запись что и выше
+function prom(mas) {
+  const TIME = 2000; // дефолт интервала
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(mas);
+    }, TIME);
+  }); // промис БЕЗ обработки ощибки
+}
+prom(promises).then(x => console.log(x)); // результат и вызов промиса
