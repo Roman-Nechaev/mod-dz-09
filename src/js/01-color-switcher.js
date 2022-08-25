19e44a94c64a11d0728d71ad18c57515f2fa997f
Roman-Nechaev <nrchaevroman@gmail.com>
2022-08-25 19:59:06 +0300

    tack_1_ok

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,42 +1,80 @@
-//  <button type="button" data-start>Start</button>
-//     <button type="button" data-stop>Stop</button>
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
-const bgPlast = document.body.style.background;
+const bgPlast = document.body;
+
+let timerId = null;
+const DELAY = 1000;
+let check = btnStop.setAttribute('disabled', false);
+////
 
 btnStart.addEventListener('click', onbtnStart);
 btnStop.addEventListener('click', onBtnStop);
-
-function onbtnStart(evt) {
-  // onIntervsl();
-  if (!onIntervsl) {
-    btnStart.setAttribute('disabled', true);
-  } else {
-    const foo = btnStart.removeAttribute('disabled');
-
-    onBtnStop(foo);
-  }
-}
-
-function onIntervsl() {
-  const intervalId = setInterval(onGenerColorsBG, 1000);
-}
-
-function onBtnStop(evt) {
-  // clearInterval(intervalId);
-  console.log('BtnStop');
-  btnStop.setAttribute('disabled', true);
-}
-
-function name(params) {
-  console.log('start');
-}
 
 // генерация случайного цвета
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-//
-function onGenerColorsBG(params) {
-  const generdColorBg = (document.body.style.background = getRandomHexColor());
+// кнопка включения START
+function onbtnStart() {
+  btnStart.setAttribute('disabled', true);
+  btnStop.removeAttribute('disabled');
+  onIntervsl();
 }
+// кнопка выключения STOP
+function onBtnStop() {
+  check = btnStop.setAttribute('disabled', true);
+  btnStart.removeAttribute('disabled');
+
+  clearInterval(timerId);
+}
+//функция интервала
+function onIntervsl() {
+  timerId = setInterval(onGenerColorsBG, DELAY);
+}
+
+// функция меняет цвет BG
+function onGenerColorsBG() {
+  storColor = bgPlast.style.background = getRandomHexColor();
+
+  localSet = localStorage.setItem('storColor', JSON.stringify(storColor));
+}
+
+/// localStorage просто практики ради
+const localGet = localStorage.getItem('storColor');
+
+const parsed = JSON.parse(localGet);
+
+const bar = (bgPlast.style.background = parsed);
+
+/* Второй Вариант  */
+/* btnStart.addEventListener('click', () => {
+  interval.start();
+});
+
+btnStop.addEventListener('click', () => {
+  interval.stop();
+});
+
+const interval = {
+  timerId: null,
+  isActiv: false,
+  start() {
+    if (this.isActiv) {
+      return;
+    }
+    this.timerId = setInterval(() => {
+      this.isActiv = true;
+      const color = getRandomHexColor();
+      bgPlast.style.backgroundColor = color;
+      btnStart.setAttribute('disabled', true);
+      btnStop.removeAttribute('disabled');
+    }, DELAY);
+  },
+  stop() {
+    clearInterval(this.timerId);
+    this.isActiv = false;
+    check = btnStop.setAttribute('disabled', true);
+    btnStart.removeAttribute('disabled');
+  },
+};
+ */
