1dd34044d66d3030904ad727038f2b508cc100ad
Roman-Nechaev <nrchaevroman@gmail.com>
2022-08-25 10:32:21 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,23 +2,38 @@
 //     <button type="button" data-stop>Stop</button>
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
+const bgPlast = document.body.style.background;
 
 btnStart.addEventListener('click', onbtnStart);
 btnStop.addEventListener('click', onBtnStop);
-let intervalId = '';
+
 function onbtnStart(evt) {
-  onIntervsl();
-  // console.log('btnStart:', evt.target);
+  // onIntervsl();
+  if (!onIntervsl()) {
+    btnStart.setAttribute('disabled', true);
+  } else {
+    btnStart.removeAttribute('disabled');
+  }
 }
 
 function onIntervsl() {
-  const intervalId = setInterval(name, 1000);
+  const intervalId = setInterval(onGenerColorsBG, 1000);
 }
 function onBtnStop(evt) {
-  clearInterval(intervalId);
+  // clearInterval(intervalId);
   console.log('BtnStop');
+  btnStop.setAttribute('disabled', true);
 }
 
 function name(params) {
   console.log('start');
+}
+
+// генерация случайного цвета
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+//
+function onGenerColorsBG(params) {
+  const generdColorBg = (document.body.style.background = getRandomHexColor());
 }
