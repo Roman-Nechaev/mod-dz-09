83097c97f886ca08efd0760b8c39801716db3ba5
Roman-Nechaev <nrchaevroman@gmail.com>
2022-08-29 16:58:11 +0300

    ew

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,7 +2,7 @@ import flatpickr from 'flatpickr';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 import 'flatpickr/dist/flatpickr.min.css';
-
+const flatpickrInpet = document.querySelector('input#datetime-picker');
 const refs = {
   input: document.querySelector('input[type="text"]'),
 
@@ -14,15 +14,6 @@ const refs = {
   seconds: document.querySelector('[data-seconds]'),
 };
 
-console.log('days', refs.days.textContent);
-console.log('hours', refs.hours.textContent);
-console.log('minutes', refs.minutes.textContent);
-console.log('seconds', refs.seconds.textContent);
-
-// console.log('refs', refs.input);
-// console.log('refs', refs.btn);
-// console.log('refs', refs.timer);
-
 let checkbtnStop = refs.btn.setAttribute('disabled', true);
 
 var countDownDate = '';
@@ -31,24 +22,36 @@ refs.input.addEventListener('input', onInputValue);
 
 refs.btn.addEventListener('click', onStartTimerBtn);
 
-flatpickr('input[type="text"]', onflatpickr);
+// flatpickr('input[type="text"]', onflatpickr);
 
-function onflatpickr(evt) {
-  refs.input.value = evt.defaultDate.toLocaleString();
-  console.log(evt.onClose.selectedDates);
-}
+// function onflatpickr(evt) {
+//   refs.input.value = evt.defaultDate.toLocaleString();
+//   console.log(evt.onClose.selectedDates);
+// }
 
-const options = {
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   dateFormat: 'Y-m-d H:i',
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   },
+// };
+
+// onflatpickr(options);
+
+const fp = flatpickr(flatpickrInpet, {
   enableTime: true,
+  dateFormat: 'Y-m-d H:i',
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
   },
-};
-
-onflatpickr(options);
+});
 
 const timer = {
   intervalId: null,
@@ -64,10 +67,13 @@ const timer = {
     this.intervalId = setInterval(() => {
       const diffDate = countDownDate - new Date();
       if (diffDate <= 0) {
-        console.log('f');
         clearInterval(this.intervalId);
+        this.isActiv = false;
+
+        Notify.failure('Timer is DONE!!!!');
       }
       const countTimer = convertMs(diffDate);
+
       console.log('countTimer', countTimer);
       updateTimerFace(countTimer);
     }, 1000);
@@ -85,7 +91,7 @@ function updateTimerFace({ days, hours, minutes, seconds }) {
 function onInputValue(evt) {
   var d = new Date(); // сегодняшная дата
 
-  countDownDate = new Date(evt.currentTarget.value);
+  countDownDate = new Date(fp.selectedDates);
 
   if (countDownDate > d) {
     console.log('Da');
@@ -129,3 +135,118 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
+
+/////////////////// ВТОРОЙ ВАРИАНТ CLASS ///////////////////////////////
+
+/* import flatpickr from 'flatpickr';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'flatpickr/dist/flatpickr.min.css';
+const flatpickrInpet = document.querySelector('input#datetime-picker');
+
+const refs = {
+  bntStart: document.querySelector('button[data-start]'),
+  $days: document.querySelector('.value[data-days]'),
+  $hours: document.querySelector('.value[data-hours]'),
+  $minutes: document.querySelector('.value[data-minutes]'),
+  $seconds: document.querySelector('.value[data-seconds]'),
+};
+
+const fp = flatpickr(flatpickrInpet, {
+  enableTime: true,
+  dateFormat: 'Y-m-d H:i',
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+});
+
+refs.bntStart.addEventListener('click', onBtnStart);
+refs.bntStart.setAttribute('disabled', true);
+flatpickrInpet.addEventListener('change', onStartTimer);
+
+class Timer {
+  constructor({ onTick }) {
+    this.timerId = null;
+    this.isActive = false;
+    this.onTick = onTick;
+  }
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+
+    const countDownDate = new Date(fp.selectedDates).getTime();
+    this.isActive = true;
+
+    this.timerId = setInterval(() => {
+      const currentTime = new Date().getTime();
+      const deltaTime = countDownDate - currentTime;
+      const onTime = this.convertMs(deltaTime);
+      this.onTick(onTime);
+      // console.log(onTime);
+
+      if (deltaTime <= 0) {
+        clearInterval(this.timerId);
+        this.isActive = false;
+        const onTime = this.convertMs(0);
+        this.onTick(onTime);
+      }
+    }, 1000);
+  }
+
+  convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = this.addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = this.addLeadingZero(
+      Math.floor(((ms % day) % hour) / minute)
+    );
+    // Remaining seconds
+    const seconds = this.addLeadingZero(
+      Math.floor((((ms % day) % hour) % minute) / second)
+    );
+
+    return { days, hours, minutes, seconds };
+  }
+  addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  }
+}
+
+const timer = new Timer({
+  onTick: timeText,
+});
+
+function onBtnStart() {
+  timer.start();
+}
+
+function onStartTimer() {
+  if (new Date(fp.selectedDates).getTime() <= new Date().getTime()) {
+    // window.alert('Please choose a date in the future');
+    Notify.failure('Please choose a date in the future');
+    refs.bntStart.setAttribute('disabled', true);
+  }
+  if (new Date(fp.selectedDates).getTime() > new Date().getTime()) {
+    refs.bntStart.removeAttribute('disabled');
+  }
+}
+
+function timeText({ days, hours, minutes, seconds }) {
+  refs.$days.textContent = `${days}`;
+  refs.$hours.textContent = `${hours}`;
+  refs.$minutes.textContent = `${minutes}`;
+  refs.$seconds.textContent = `${seconds}`;
+}
+ */
