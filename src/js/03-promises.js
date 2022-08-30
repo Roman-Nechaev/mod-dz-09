1520fb8f697a8f831eb700d8bfa0d06bdee0c8f7
Roman-Nechaev <nrchaevroman@gmail.com>
2022-08-30 20:59:06 +0300

    vc

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,27 +1,68 @@
 refs = {
   form: document.querySelector('.form'),
+  input: document.querySelector('input'),
   btn: document.querySelector('button'),
 };
 console.log(refs.form);
+console.log(refs.input);
 console.log(refs.btn);
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+refs.form.addEventListener('submit', onSobmitForm);
+// refs.btn.addEventListener('click', onBtn);
+//////////////////////////////////////////////////
+
+function onSobmitForm(e) {
+  e.preventDefault();
+
+  let { delay, step, amount } = e.currentTarget;
+
+  console.log(delay.value);
+
+  // console.log(step.value);
+  // console.log(amount.value);
+  onBtn(step.value, amount.value);
+}
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  return new Promise((resolve, reject) => {});
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setInterval(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+function funcBefore() {
+  createPromise(2, 2500)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+}
+
+function onBtn(step, amount) {
+  count = 0; // начальное значение повтореений
+  console.log(amount);
+  console.log(step);
+  intervalId = setInterval(function () {
+    count += 1;
+    if (count == amount) {
+      clearInterval(intervalId);
+    }
+    funcBefore();
+  }, step);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // const mass = ['Roman', 'Ragnar', 'Thor', 'Odin']; // массив объектов
 // const promises = mass.map(prom); // возвращает массив объектов
 // // const promises = mass.map(mas => prom(mas)); // идентичная запись что и выше
